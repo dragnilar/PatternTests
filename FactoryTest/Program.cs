31b04f5c5e2fb3de66304afc746becb6256bbcc8
Great Dead Cthulhu <dragnilar@gmail.com>
2018-04-29 01:11:29 -0400

    Add project files.

---
+++ FactoryTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
