6901af49a8fe18998e6ee1da3442856939d1bf75
Great Dead Cthulhu <dragnilar@gmail.com>
2018-07-19 03:12:15 -0400

    Added strategy pattern test

---
+++ StrategyTest/Classes/BasicStrategyExample.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyTest.Classes
+{
+    public class BasicStrategyExample
+    {
+        public void ShowExample()
+        {
+            Console.Clear();
+            Console.WriteLine("Begin basic strategy example.");
+
+            var context = new BasicStrategyContext(new Strategy1());
+            context.ContextInterface();
+            context = new BasicStrategyContext(new Strategy2());
+            context.ContextInterface();
+            context = new BasicStrategyContext(new Strategy3());
+            context.ContextInterface();
+
+            Console.WriteLine("Finished basic strategy example.");
+            Console.ReadKey();
+            Console.Clear();
+
+        }
+
+        public class BasicStrategyContext
+        {
+            private BasicStrategy _basicStrategy;
+
+            public BasicStrategyContext(BasicStrategy strategy)
+            {
+                this._basicStrategy = strategy;
+            }
+
+            public void ContextInterface()
+            {
+                _basicStrategy.AlgorithmInterface();
+            }
+        }
+
+        public abstract class BasicStrategy
+        {
+            public abstract void AlgorithmInterface();
+        }
+        public class Strategy1 : BasicStrategy {
+            public override void AlgorithmInterface()
+            {
+                Console.WriteLine($"Called {this.GetType().Name}'s Algorithm Interface");
+            }
+        }
+        public class Strategy2 : BasicStrategy {
+            public override void AlgorithmInterface()
+            {
+                Console.WriteLine($"Called {this.GetType().Name}'s Algorithm Interface");
+            }
+        }
+        public class Strategy3 : BasicStrategy {
+            public override void AlgorithmInterface()
+            {
+                Console.WriteLine($"Called {this.GetType().Name}'s Algorithm Interface");
+            }
+        }
+    }
+}
---
+++ StrategyTest/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StrategyTest.Classes;
+
+namespace StrategyTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainMenu();
+        }
+
+        static void MainMenu()
+        {
+            while (true)
+            {
+                Console.WriteLine("Press 1 to Run Basic Strategy Test");
+                Console.WriteLine("Press Esc to Exit");
+
+                while (true)
+                {
+                    var input = Console.ReadKey();
+
+                    switch (input.Key)
+                    {
+                        case ConsoleKey.D1:
+                        case ConsoleKey.NumPad1:
+                            new BasicStrategyExample().ShowExample();
+                            break;
+                        case ConsoleKey.Escape:
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            Console.Clear();
+                            break;
+                    }
+
+                    break;
+                }
+            }
+        }
+    }
+}
