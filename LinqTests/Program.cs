29a303917d017d47cb1aa3390ee477644601d4d8
Great Dead Cthulhu <dragnilar@gmail.com>
2018-07-30 12:04:25 -0400

    Linq test stub

---
+++ LinqTests/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainMenu();
+        }
+
+        private static void MainMenu()
+        {
+            ShowMenuOptions();
+        }
+
+        private static void ShowMenuOptions()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
